562ac2f736423eb0eca2ef1fd846dc893b709688
jarek1725 <tomaszewskijarek1725@gmail.com>
2023-03-20 23:05:51 +0100

    Refactor move code

--- src/main/java/org/example/Models/Board.java
+++ src/main/java/org/example/Models/Board.java
@@ -6,49 +6,60 @@ public class Board {
     private final Position[][] positions = new Position[8][8];
     private boolean isWhiteTurn = false;
     private boolean isEnd = false;
-
     private final Scanner scanner = new Scanner(System.in);
 
-    public boolean makeMove(Position from, Position to) {
-        if (from.getChessman().canMove(from, to) && from.getChessman().isWhite() == isWhiteTurn) {
-            to.setChessman(from.getChessman());
-            from.setChessman(null);
-            isWhiteTurn = !isWhiteTurn;
-            drawBoard(Collections.emptyList());
-            return true;
-        } else
-            drawBoard(List.of("Invalid move"));
-        return false;
+    public void makeMove(Position from, Position to) {
+        to.setChessman(from.getChessman());
+        from.setChessman(null);
+        isWhiteTurn = !isWhiteTurn;
+        drawBoard(Collections.emptyList());
     }
 
     public void startGame() {
         drawBoard(Collections.emptyList());
-
-        System.out.print("Select chessman to move, for example 'A2': ");
-        String positionStartCoords = scanner.nextLine();
-        Position positionStart = getChessmanAtPosition(positionStartCoords);
-        if (positionStart.getChessman() == null) {
-            drawBoard(List.of("No chessman at this position"));
-            return;
-        } else {
-            List<String> availableMoves = positionStart.getChessman().getAvailableMoves(positionStart);
-            if (availableMoves.isEmpty()) {
-                drawBoard(List.of("No available moves"));
-                return;
+        Position positionStart = null;
+        while (!isEnd) {
+            positionStart = selectChessman();
+            if (positionStart != null) {
+                operationsOnSelectedChessman(positionStart);
             } else {
-                drawBoard(List.of("Available moves: " + availableMoves.stream().reduce((s, s2) -> s + ", " + s2).get()));
-                System.out.print("Select position to move to, for example 'A3': ");
-                String positionEndCoords = scanner.nextLine();
-                Position positionEnd = getChessmanAtPosition(positionEndCoords);
-                if (availableMoves.contains(positionEndCoords)) {
-                    makeMove(positionStart, positionEnd);
-                } else {
-                    drawBoard(List.of("Invalid move"));
-                }
+                drawBoard(List.of("Invalid chessman"));
             }
         }
+    }
 
-//        makeMove(getChessmanAtPosition(positionStart), getChessmanAtPosition("A5"));
+    private void operationsOnSelectedChessman(Position positionStart) {
+        List<String> availableMoves = positionStart.getChessman().getAvailableMoves(positionStart);
+        if (availableMoves.isEmpty()) {
+            drawBoard(List.of("No available moves"));
+        } else {
+            drawBoard(List.of("Available moves: " + availableMoves.stream().reduce((s, s2) -> s + ", " + s2).get()));
+            selectPositionEnd(availableMoves, positionStart);
+        }
+    }
+
+    private Position selectChessman() {
+        System.out.print("Select chessman to move, for example 'A2': ");
+        String positionStartCoords = scanner.nextLine();
+        return isSelectedChessmanValid(positionStartCoords) ? getChessmanAtPosition(positionStartCoords) : null;
+    }
+
+    private boolean isSelectedChessmanValid(String positionStartCoords) {
+        return getChessmanAtPosition(positionStartCoords) != null
+                && getChessmanAtPosition(positionStartCoords).getChessman() != null
+                && !isEnd
+                && getChessmanAtPosition(positionStartCoords).getChessman().isWhite() == isWhiteTurn;
+    }
+
+    private void selectPositionEnd(List<String> availableMoves, Position positionStart) {
+        System.out.print("Select position to move to, for example 'A3': ");
+        String positionEndCoords = scanner.nextLine();
+        Position chessmanAtPosition = getChessmanAtPosition(positionEndCoords);
+        if (availableMoves.contains(positionEndCoords)) {
+            makeMove(positionStart, chessmanAtPosition);
+        } else {
+            drawBoard(List.of("Invalid move"));
+        }
     }
 
     private List<String> getAvailableMoves(Position positionStart) {
@@ -56,9 +67,15 @@ public class Board {
     }
 
     public Position getChessmanAtPosition(String position) {
-        int x = Character.toUpperCase(position.charAt(0)) - 65;
-        int y = Integer.parseInt(position.substring(1)) - 1;
-        return Arrays.stream(positions).filter(row -> row[y].getX() == y).findFirst().get()[x];
+        try {
+            int x = Character.toUpperCase(position.charAt(0)) - 65;
+            int y = Integer.parseInt(position.substring(1)) - 1;
+            return Arrays.stream(positions).filter(row -> row[y].getX() == y).findFirst().get()[x];
+        } catch (Exception e) {
+            drawBoard(List.of("Invalid position"));
+        }
+        ;
+        return null;
     }
 
 
